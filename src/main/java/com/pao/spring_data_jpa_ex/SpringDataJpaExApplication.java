4a718e45d875ca9f4e0ed0491d467b861fefdae5
Nattawut visuttipinat <nattawut.visuttipinat@aia.com>
2025-02-02 18:57:56 +0700

    refactor

--- src/main/java/com/pao/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ src/main/java/com/pao/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -11,27 +11,6 @@ public class SpringDataJpaExApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaExApplication.class, args);
-//		ApplicationContext context = SpringApplication.run(SpringDataJpaExApplication.class, args);
-
-//		StudentRepo repo = context.getBean(StudentRepo.class);
-
-
-
-//		Student s1 = context.getBean(Student.class);
-//		Student s2 = context.getBean(Student.class);
-//		Student s3 = context.getBean(Student.class);
-//
-//		s1.setName("pao1");
-//		s1.setMarks(1);
-//		s1.setRollNo(10);
-//
-//		repo.save(s1);
-
-//		s2.setName("pao3");
-//		s2.setMarks(1);
-//		s2.setRollNo(10);
-//
-//		repo.delete(s2);
 
 	}
 
